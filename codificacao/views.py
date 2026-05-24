4dac0814ad668ba9be3602526236150d662be214
josematheus7 <math.filho1996@gmail.com>
2026-05-24 19:57:01 -0300

    commit

--- codificacao/views.py
+++ codificacao/views.py
@@ -1,6 +1,6 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect
 from main import app
-from dbbanco import *
+from codificacao.dbquery import *
 
 # ROTA PRODUTOS ###################################
 @app.route('/')
